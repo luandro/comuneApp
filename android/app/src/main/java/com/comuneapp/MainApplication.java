57b04ff7aef98e3751e0b24321f4daf7fa06c783
luandro <luandro@gmail.com>
2020-06-29 20:56:14 -0300

    added react-native-sockets

--- android/app/src/main/java/com/comuneapp/MainApplication.java
+++ android/app/src/main/java/com/comuneapp/MainApplication.java
@@ -11,7 +11,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 
-import com.stonem.sockets.SocketsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new SocketsPackage());
           return packages;
         }
 
